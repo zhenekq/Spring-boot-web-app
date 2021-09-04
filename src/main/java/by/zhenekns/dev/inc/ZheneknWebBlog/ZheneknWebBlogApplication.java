774aa1329ad41794a5bc5539dd51910369b66d4c
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-09-04 11:43:36 +0300

    initial files

---
+++ src/main/java/by/zhenekns/dev/inc/ZheneknWebBlog/ZheneknWebBlogApplication.java
@@ -0,0 +1,13 @@
+package by.zhenekns.dev.inc.ZheneknWebBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZheneknWebBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZheneknWebBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/zhenekns/dev/inc/ZheneknWebBlog/ZheneknWebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zhenekns.dev.inc.ZheneknWebBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZheneknWebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
