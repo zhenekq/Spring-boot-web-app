913712dda86d64e4cabf39d79db236279c755593
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-09-04 12:17:00 +0300

    added main page and mapping

---
+++ src/main/java/by/zhenekns/dev/inc/ZheneknWebBlog/controller/BlogController.java
@@ -0,0 +1,16 @@
+package by.zhenekns.dev.inc.ZheneknWebBlog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+   @GetMapping("/blog")
+    public String blogMain(Model model){
+
+   }
+
+
+}
--- src/main/java/by/zhenekns/dev/inc/ZheneknWebBlog/controller/MainController.java
+++ src/main/java/by/zhenekns/dev/inc/ZheneknWebBlog/controller/MainController.java
@@ -3,6 +3,7 @@ package by.zhenekns.dev.inc.ZheneknWebBlog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class MainController {
@@ -10,6 +11,11 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Main page");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
         return "home";
     }
 
